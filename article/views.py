11dc9d0638645de4f9bad18949476e1ca47dab37
sguotciq <sguotciq@163.com>
2015-11-09 17:04:55 +0800

    分类 归档 添加分页

--- article/views.py
+++ article/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def default(request):
@@ -14,8 +15,20 @@ def default(request):
 def test(request) :
     return render(request, 'test.html', {'current_time': datetime.now()})
 
+# def home(request):
+#     post_list = Article.objects.all()  #获取全部的Article对象
+#     return render(request, 'home.html', {'post_list' : post_list})
+
 def home(request):
-    post_list = Article.objects.all()  #获取全部的Article对象
+    posts = Article.objects.all()  #获取全部的Article对象
+    paginator = Paginator(posts, 3) #每页显示两个
+    page = request.GET.get('page')
+    try :
+        post_list = paginator.page(page)
+    except PageNotAnInteger :
+        post_list = paginator.page(1)
+    except EmptyPage :
+        post_list = paginator.paginator(paginator.num_pages)
     return render(request, 'home.html', {'post_list' : post_list})
 
 # def detail(request, my_args):
@@ -30,3 +43,36 @@ def detail(request, id):
     except Article.DoesNotExist:
         raise Http404
     return render(request, 'post.html', {'post': post})
+
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list, 
+                                            'error' : False})
+
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+def search_tag(request, tag) :
+    try:
+        post_list = Article.objects.filter(category__iexact = tag) #contains
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'tag.html', {'post_list' : post_list})
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = s)
+            if len(post_list) == 0 :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : True})
+            else :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : False})
+    return redirect('/')
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -18,9 +18,14 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'article.views.default', name='default'),
+    url(r'^home/$', 'article.views.default', name='default'),
     # url(r'^(?P<my_args>\d+)/$', 'article.views.detail', name='detail'),
     url(r'^test/$', 'article.views.test', name='test'),
-    url(r'^home/$', 'article.views.home', name='home'),
+    # url(r'^home/$', 'article.views.home', name='home'),
+    url(r'^$', 'article.views.home', name='home'),    
     url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^archives/$', 'article.views.archives', name = 'archives'),
+    url(r'^aboutme/$', 'article.views.about_me', name = 'about_me'),
+    url(r'^tag(?P<tag>\w+)/$', 'article.views.search_tag', name = 'search_tag'),
+    url(r'^search/$','article.views.blog_search', name = 'search'),
 ]
